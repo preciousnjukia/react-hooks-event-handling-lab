d0f2365f84da78730c7bf3c0eb5b0efeaec0bc83
preciousnjukia <preciousnjukia@gmail.com>
2023-09-01 16:09:00 +0300

    Assignment complete

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Keypad from "./Keypad";
+import EyesOnMe from "./EyesOnMe";
+
+function App(){
+    return(
+        <div>
+            <Keypad />
+            <EyesOnMe />
+        </div>
+    );
+}
+
+export default App;
